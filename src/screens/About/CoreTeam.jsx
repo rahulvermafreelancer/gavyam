b63d1c9ac924d950799126a89867fd5f2e4de811
rahulvermafreelancer <rahulvermafreelancer@gmail.com>
2022-07-05 20:07:55 +0530

    added flip card

--- src/screens/About/CoreTeam.jsx
+++ src/screens/About/CoreTeam.jsx
@@ -1,120 +1,225 @@
 import React from "react";
 import "./index.css";
+import "./team.css";
 
 export const CoreTeam = () => {
   return (
     <div className="core-team-main">
       <h1 className="core-team-heading font-regular font-bold">Core Team</h1>
 
-      <div className="team-card-main">
-        <div className="team-card-body">
-          <div className="team-card-heading">
-            <p className="heading-Text font-regular">Director</p>
-          </div>
-          <div className="team-user-section">
-            <img
-              src="./assets/about/test.jfif"
-              alt="user"
-              className="team-image-radius"
-            />
-          </div>
-          <div className="team-user-name">
-            <p className="team-name-Text font-regular">Nithya Nair</p>
-            <div className="icon-section">
-              <img src="./assets/about/plus.svg" alt="plus" />
+      <div
+        style={{
+          width: "80%",
+          margin: "0 auto",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
+          flexWrap: "wrap"
+        }}
+      >
+        <div class="flip-card" style={{ marginBottom: 40 }}>
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <div className="designation-div">
+                <p className="font-regular designation-font">Director</p>
+              </div>
+              <div className="card-image-dive">
+                <img
+                  src="./assets/about/test.jfif"
+                  alt="user"
+                  className="team-card-image-div"
+                />
+              </div>
+              <div className="name-div">
+                <p className="team-name-Text font-regular">Nithya Nair</p>
+                <div className="icon-section">
+                  <img src="./assets/about/plus.svg" alt="plus" />
+                </div>
+              </div>
+            </div>
+            <div class="flip-card-back">
+              <p className="font-regular team-card-para-text">
+                Team is the core strength that builds a dream into a mission,
+                and a mission into revolution. Our incredibly talented team
+                works tirelessly to ease up every dairy farmer's life with
+                digitalization and optimize cattle management and breeding with
+                innovative solutions. Each member of our team brings different
+                skillset to the table, and their ideologies aligned for one
+                single vision is what makes us best in the business.
+              </p>
             </div>
           </div>
         </div>
 
-        <div className="team-card-body">
-          <div className="team-card-heading">
-            <p className="heading-Text font-regular">Director</p>
-          </div>
-          <div className="team-user-section">
-            <img
-              src="./assets/about/test.jfif"
-              alt="user"
-              className="team-image-radius"
-            />
-          </div>
-          <div className="team-user-name">
-            <p className="team-name-Text font-regular">Rajesh Patel</p>
-            <div className="icon-section">
-              <img src="./assets/about/plus.svg" alt="plus" />
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <div className="designation-div">
+                <p className="font-regular designation-font">Director</p>
+              </div>
+              <div className="card-image-dive">
+                <img
+                  src="./assets/about/test.jfif"
+                  alt="user"
+                  className="team-card-image-div"
+                />
+              </div>
+              <div className="name-div">
+                <p className="team-name-Text font-regular">Rajesh Patel</p>
+                <div className="icon-section">
+                  <img src="./assets/about/plus.svg" alt="plus" />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="team-card-body">
-          <div className="team-card-heading">
-            <p className="heading-Text font-regular">Director</p>
-          </div>
-          <div className="team-user-section">
-            <img
-              src="./assets/about/test.jfif"
-              alt="user"
-              className="team-image-radius"
-            />
-          </div>
-          <div className="team-user-name">
-            <p className="team-name-Text font-regular">Dipen Parmar</p>
-            <div className="icon-section">
-              <img src="./assets/about/plus.svg" alt="plus" />
-            </div>
-          </div>
-        </div>
-
-        <div className="team-card-body">
-          <div className="team-card-heading">
-            <p className="heading-Text font-regular">CTO</p>
-          </div>
-          <div className="team-user-section">
-            <img
-              src="./assets/about/test.jfif"
-              alt="user"
-              className="team-image-radius"
-            />
-          </div>
-          <div className="team-user-name">
-            <p className="team-name-Text font-regular">Tejas Vaghela</p>
-            <div className="icon-section">
-              <img src="./assets/about/plus.svg" alt="plus" />
+            <div class="flip-card-back">
+              <p className="font-regular team-card-para-text">
+                Team is the core strength that builds a dream into a mission,
+                and a mission into revolution. Our incredibly talented team
+                works tirelessly to ease up every dairy farmer's life with
+                digitalization and optimize cattle management and breeding with
+                innovative solutions. Each member of our team brings different
+                skillset to the table, and their ideologies aligned for one
+                single vision is what makes us best in the business.
+              </p>
             </div>
           </div>
         </div>
 
-        <div className="team-card-body">
-          <div className="team-card-heading">
-            <p className="heading-Text font-regular">COO</p>
-          </div>
-          <div className="team-user-section">
-            <img
-              src="./assets/about/test.jfif"
-              alt="user"
-              className="team-image-radius"
-            />
-          </div>
-          <div className="team-user-name">
-            <p className="team-name-Text font-regular">Sreekanth Naidu</p>
-            <div className="icon-section">
-              <img src="./assets/about/plus.svg" alt="plus" />
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <div className="designation-div">
+                <p className="font-regular designation-font">Director</p>
+              </div>
+              <div className="card-image-dive">
+                <img
+                  src="./assets/about/test.jfif"
+                  alt="user"
+                  className="team-card-image-div"
+                />
+              </div>
+              <div className="name-div">
+                <p className="team-name-Text font-regular">Dipen Parmar</p>
+                <div className="icon-section">
+                  <img src="./assets/about/plus.svg" alt="plus" />
+                </div>
+              </div>
+            </div>
+            <div class="flip-card-back">
+              <p className="font-regular team-card-para-text">
+                Team is the core strength that builds a dream into a mission,
+                and a mission into revolution. Our incredibly talented team
+                works tirelessly to ease up every dairy farmer's life with
+                digitalization and optimize cattle management and breeding with
+                innovative solutions. Each member of our team brings different
+                skillset to the table, and their ideologies aligned for one
+                single vision is what makes us best in the business.
+              </p>
             </div>
           </div>
         </div>
 
-        <div className="team-card-body">
-          <h1 className="question-heading font-regular font-bold">
-            Have a Question?
-          </h1>
-          <p className="question-para font-regular">
-            In case you haven't found the answer to your question, please feel
-            free to
-            <span style={{ color: "#B55219" }}>
-              <a href="/contact" style={{ textDecoration: "none" }}>
-                contact us
-              </a>
-            </span>
-            , our customer support will be happy to help you.
-          </p>
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <div className="designation-div">
+                <p className="font-regular designation-font">CTO</p>
+              </div>
+              <div className="card-image-dive">
+                <img
+                  src="./assets/about/test.jfif"
+                  alt="user"
+                  className="team-card-image-div"
+                />
+              </div>
+              <div className="name-div">
+                <p className="team-name-Text font-regular">Tejas Vaghela</p>
+                <div className="icon-section">
+                  <img src="./assets/about/plus.svg" alt="plus" />
+                </div>
+              </div>
+            </div>
+            <div class="flip-card-back">
+              <p className="font-regular team-card-para-text">
+                Team is the core strength that builds a dream into a mission,
+                and a mission into revolution. Our incredibly talented team
+                works tirelessly to ease up every dairy farmer's life with
+                digitalization and optimize cattle management and breeding with
+                innovative solutions. Each member of our team brings different
+                skillset to the table, and their ideologies aligned for one
+                single vision is what makes us best in the business.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <div className="designation-div">
+                <p className="font-regular designation-font">COO</p>
+              </div>
+              <div className="card-image-dive">
+                <img
+                  src="./assets/about/test.jfif"
+                  alt="user"
+                  className="team-card-image-div"
+                />
+              </div>
+              <div className="name-div">
+                <p className="team-name-Text font-regular">Sreekanth Naidu</p>
+                <div className="icon-section">
+                  <img src="./assets/about/plus.svg" alt="plus" />
+                </div>
+              </div>
+            </div>
+            <div class="flip-card-back">
+              <p className="font-regular team-card-para-text">
+                Team is the core strength that builds a dream into a mission,
+                and a mission into revolution. Our incredibly talented team
+                works tirelessly to ease up every dairy farmer's life with
+                digitalization and optimize cattle management and breeding with
+                innovative solutions. Each member of our team brings different
+                skillset to the table, and their ideologies aligned for one
+                single vision is what makes us best in the business.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <h1 className="question-heading font-regular font-bold">
+                Have a Question?
+              </h1>
+              <p className="question-para font-regular">
+                In case you haven't found the answer to your question, please
+                feel free to
+                <span style={{ color: "#B55219" }}>
+                  <a href="/contact" style={{ textDecoration: "none" }}>
+                    contact us
+                  </a>
+                </span>
+                , our customer support will be happy to help you.
+              </p>
+            </div>
+            <div class="flip-card-back">
+              <h1 className="question-heading font-regular font-bold">
+                Have a Question?
+              </h1>
+              <p className="question-para font-regular">
+                In case you haven't found the answer to your question, please
+                feel free to
+                <span style={{ color: "#B55219" }}>
+                  <a href="/contact" style={{ textDecoration: "none" }}>
+                    contact us
+                  </a>
+                </span>
+                , our customer support will be happy to help you.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
